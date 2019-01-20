52bcb9b3bdcb84132b224d8314e171486b8d3943
Alan Ramos <alan.ramos@jarsa.com.mx>
2019-01-20 07:10:53 -0600

    [REF] sale_supplier_line: use self instead of using a variable

--- addons/sale_supplier_line/models/sale_order_line.py
+++ addons/sale_supplier_line/models/sale_order_line.py
@@ -14,13 +14,14 @@ class SaleOrderLine(models.Model):
         'res.currency', string='Purchase Currency')
 
     @api.multi
-    def check_lines(self, values, rec):
-        product = rec.product_id
-        vendor_id = rec.vendor_id.id
-        purchase_price = rec.purchase_price
-        currency_id = rec.purchase_currency_id.id
+    def check_lines(self, values):
+        self.ensure_one()
+        product = self.product_id
+        vendor_id = self.vendor_id.id
+        purchase_price = self.purchase_price
+        currency_id = self.purchase_currency_id.id
         if 'product_id' in values:
-            product = rec.product_id.browse(values['product_id'])
+            product = self.product_id.browse(values['product_id'])
         if 'vendor_id' in values:
             vendor_id = values['vendor_id']
         if 'purchase_price' in values:
@@ -28,7 +29,7 @@ class SaleOrderLine(models.Model):
         if 'purchase_currency_id' in values:
             currency_id = values['purchase_currency_id']
         suppinfo = product.seller_ids.with_context(
-            vendor_id=vendor_id, sale_order_id=rec.order_id.id).filtered(
+            vendor_id=vendor_id, sale_order_id=self.order_id.id).filtered(
                 lambda l: l.name.id == l._context['vendor_id'] and
                 l.sale_order_id.id == l._context['sale_order_id'])
         vals = {
@@ -37,7 +38,7 @@ class SaleOrderLine(models.Model):
             'product_tmpl_id': product.product_tmpl_id.id,
             'price': purchase_price,
             'currency_id': currency_id,
-            'sale_order_id': rec.order_id.id,
+            'sale_order_id': self.order_id.id,
             'delay': 1.0,
             'min_qty': 1.0,
         }
@@ -53,16 +54,15 @@ class SaleOrderLine(models.Model):
                     not values.get('purchase_currency_id') and
                     not values.get('purchase_price')):
                 continue
-            rec.check_lines(values, rec)
+            rec.check_lines(values)
         return super().write(values)
 
     @api.model
     def create(self, values):
         record = super().create(values)
-        for rec in record:
-            if (not values.get('vendor_id') and
-                    not values.get('purchase_currency_id') and
-                    not values.get('purchase_price')):
-                continue
-            rec.check_lines(values, rec)
+        if (not values.get('vendor_id') and
+                not values.get('purchase_currency_id') and
+                not values.get('purchase_price')):
+            return record
+        record.check_lines(values)
         return record
