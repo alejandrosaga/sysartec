ceeee2d984ea40c66a48d1df3bdc845b877a3758
Alan Ramos <alan.ramos@jarsa.com.mx>
2019-02-06 18:23:09 -0600

    [ADD] sysartec: create field commitment date to sale order

---
+++ addons/sysartec/models/sale_order.py
@@ -0,0 +1,10 @@
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    commitment_date = fields.Date(required=True, string='Commitmnet Date')
