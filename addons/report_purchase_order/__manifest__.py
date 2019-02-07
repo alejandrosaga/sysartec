9fa2fd178aa0e6833ec85f7e6f525c6d27a8e33b
Alan Ramos <alan.ramos@jarsa.com.mx>
2019-02-06 18:13:47 -0600

    [ADD] report_purchase_order: custom report for purchase order

---
+++ addons/report_purchase_order/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Report: Purchase Order',
+    'summary': 'Custom report for purchase order',
+    'version': '11.0.1.0.0',
+    'category': 'Purchase',
+    'author': 'Jarsa Sistemas',
+    'website': 'https://www.jarsa.com.mx',
+    'license': 'AGPL-3',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'views/report_purchase_order.xml',
+    ],
+}
